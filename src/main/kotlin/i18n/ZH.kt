6c2b0b6c7c1d98a9458b53a0199df6c030d149f9
セリカ・シルフィル <64072399+natsumerinchan@users.noreply.github.com>
2023-02-16 10:43:12 +0800

    Update Condition (#1)
    * Update Condition
    
    https://github.com/Kernel-SU/ksu_exam/pull/3
    
    * Update ZH.kt
    
    * Add an example

--- src/main/kotlin/i18n/ZH.kt
+++ src/main/kotlin/i18n/ZH.kt
@@ -4,15 +4,17 @@ val ZH = Model(
     lang = "zh",
     problem = """
 题目：
-这里有一台内核版本为5.10.81-android12-9-xxxx-xxxx，Android版本为13的GKI设备，内核压缩方式未知，请在网站上认真翻阅教程后为该设备刷入正确版本和压缩格式的镜像。
+这里有一台内核版本为5.10.81-android12-9-xxxx-xxxx，Android版本为13的GKI 2.0设备，内核压缩方式未知，请在网站上认真翻阅教程后为该设备刷入正确版本和压缩格式的镜像。
 
 条件：
-系统状态：已进入bootloader(fastboot)
+设备状态：已进入系统
 
-terminalfaker内置命令(可键入help查看) : fastboot, magiskboot, cd, ls, cat, clear, echo, help, history, pwd, reboot, rm, touch, uname, version, whoami
+terminalfaker内置命令(可键入help查看) : status, adb, fastboot, magiskboot, cd, ls, cat, clear, echo, help, history, pwd, reboot, rm, touch, uname, version, whoami
 
 magiskboot： 这里使用的是x86_64平台的二进制文件，直接在linux终端操作即可，无需尝试将它和boot.img推送至手机。
  
+status: 用于检查设备状态(system/bootloader/fastbootd/recovery)
+
 在terminalfaker中执行任何命令都不要加上“./”
 
 目录/home/user/workdir : 本次测试所有用到的文件都被放在这个目录下，如 magiskboot, boot-offical.img 等
@@ -21,7 +23,7 @@ https://kernel-su.github.io/ksu_exam/?pwd=[PASSWORD]
 
 完成后，你将得到一个密码，对机器人发送
 
-/join [你得到的密码]
+/join [你得到的密码] (例如 /join 123456)
 
 即可进群
 """.trimIndent(),
