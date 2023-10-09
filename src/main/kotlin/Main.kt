05a81143a0a10789ab305b5f72877a0a90e03461
Ylarod <me@ylarod.cn>
2023-10-09 03:52:35 -0500

    Update Main.kt

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -14,6 +14,7 @@ import java.util.*
 import kotlin.collections.HashMap
 import kotlin.math.absoluteValue
 import kotlin.random.Random
+import MaxSizeHashMap
 
 /**
  * This is one of the most easiest bot - it will just print information about itself
